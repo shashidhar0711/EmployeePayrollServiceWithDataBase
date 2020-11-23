f9e6dd82c00ff8fa54e63de6d25d2cd30914bbeb
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-11-23 21:25:35 +0530

    [sasic] Add .  Creating Connection to DataBase

---
+++ EmployeePayrollServiceWithDataBase/EmployeeModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeePayrollServiceWithDataBase
+{
+    /// <summary>
+    /// Getters and setters
+    /// </summary>
+    public class EmployeeModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Salary { get; set; }
+        public DateTime Start { get; set; }
+        public char Gender { get; set; }
+        public string Department { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public decimal Basic_Pay { get; set; }
+        public decimal Deductions { get; set; }
+        public decimal Taxable_Pay { get; set; }
+        public decimal Income_Tax { get; set; }
+        public decimal Net_Pay { get; set; }
+    }
+}
--- EmployeePayrollServiceWithDataBase/Program.cs
+++ EmployeePayrollServiceWithDataBase/Program.cs
@@ -4,6 +4,10 @@ namespace EmployeePayrollServiceWithDataBase
 {
     public class Program
     {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Payroll Service");
