25ec58eebe9c4d025ade29bc3c0ac44b2a53ec04
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-11-23 21:14:32 +0530

    Add project files.

---
+++ EmployeePayrollServiceWithDataBase/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeePayrollServiceWithDataBase
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll Service");
+        }
+    }
+}
