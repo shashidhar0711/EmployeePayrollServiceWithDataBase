665c1c46b8cce67349a8897d13c9f7f9286fa98f
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-12-01 14:15:37 +0530

    [sasic] Refactor . Adding Employees InTo List Using Threads

--- EmployeePayrollServiceWithDataBase/EmployeePayrollOperation.cs
+++ EmployeePayrollServiceWithDataBase/EmployeePayrollOperation.cs
@@ -16,7 +16,7 @@ namespace EmployeePayrollServiceWithDataBase
         public static string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Payroll_Service;Integrated Security=True";
         SqlConnection sqlConnection = new SqlConnection(connectionString);
 
-        public List<EmployeeModel> employeePayrollList = new List<EmployeeModel>();
+        public List<EmployeeModel> employeePayrollModelList = new List<EmployeeModel>();
 
         /// <summary>
         /// UC10 wihtout using threads
@@ -28,10 +28,35 @@ namespace EmployeePayrollServiceWithDataBase
             employeePayrollList.ForEach(employeeData =>
             {
                 Console.WriteLine("Employee Being Added: " + employeeData.Name);
-                this.AddEmployee(employeeData);
+                this.AddEmployeePayroll(employeeData);
                 Console.WriteLine("Employee Added: " + employeeData.Name);
             });
-            Console.WriteLine(this.employeePayrollList.ToString());
+            Console.WriteLine(this.employeePayrollModelList.ToString());
+        }
+
+        /// <summary>
+        /// Adds the employee payroll.
+        /// </summary>
+        /// <param name="employeeModel">The employee model.</param>
+        public void AddEmployeePayroll(EmployeeModel employeeModel)
+        {
+            employeePayrollModelList.Add(employeeModel);
+        }
+
+        /// <summary>
+        /// UC10 Without using threads
+        /// Adds the employee to payroll data base.
+        /// </summary>
+        /// <param name="employeePayrollList">The employee payroll list.</param>
+        public void AddEmployeeToPayrollDataBase(List<EmployeeModel> employeePayrollList)
+        {
+            employeePayrollList.ForEach(employeeData =>
+            {
+                Console.WriteLine("Employee Being Added: " + employeeData.Name);
+                this.AddEmployeeInToDataBase(employeeData);
+                Console.WriteLine("Employee Added: " + employeeData.Name);
+            });
+            Console.WriteLine(this.employeePayrollModelList.ToString());
         }
 
         /// <summary>
@@ -40,56 +65,27 @@ namespace EmployeePayrollServiceWithDataBase
         /// <param name="model">The model.</param>
         /// <returns></returns>
         /// <exception cref="Exception"></exception>
-        public bool AddEmployee(EmployeeModel model)
+        public void AddEmployeeInToDataBase(EmployeeModel employeeDetails)
         {
-            try
-            {
-                using (this.sqlConnection)
-                {
-                    SqlCommand command = new SqlCommand("SpAddEmployeeDetails", this.sqlConnection);
-                    command.CommandType = CommandType.StoredProcedure;
-                    command.Parameters.AddWithValue("@Name", model.Name);
-                    command.Parameters.AddWithValue("@Salary", model.Salary);
-                    command.Parameters.AddWithValue("@Start", DateTime.Now);
-                    command.Parameters.AddWithValue("@Gender", model.Gender);
-                    command.Parameters.AddWithValue("@PhoneNumber", model.PhoneNumber);
-                    command.Parameters.AddWithValue("@Address", model.Address);
-                    command.Parameters.AddWithValue("@Department", model.Department);
-                    command.Parameters.AddWithValue("@Basic_Pay", model.Basic_Pay);
-                    command.Parameters.AddWithValue("@Deductions", model.Deductions);
-                    command.Parameters.AddWithValue("@Taxable_Pay", model.Taxable_Pay);
-                    command.Parameters.AddWithValue("@Income_Tax", model.Income_Tax);
-                    command.Parameters.AddWithValue("@Net_Pay", model.Net_Pay);
-                    this.sqlConnection.Open();
-                    /// It used to executing queries that does not return any data.
-                    var result = command.ExecuteNonQuery();
-                    this.sqlConnection.Close();
-                    if (result != 0)
-                    {
-                        return true;
-                    }
-                    return false;
-                }
-            }
-            catch (Exception e)
-            {
-                throw new Exception(e.Message);
-            }
-            finally
-            {
-                /// Connections closes
-                this.sqlConnection.Close();
-            }
-            return false;
-        }
 
-        /// <summary>
-        /// Adds the employee payroll into the other employee payroll
-        /// </summary>
-        /// <param name="employeeModel">The employee model.</param>
-        public void AddEmployeePayroll(EmployeeModel employeeModel)
-        {
-            employeePayrollList.Add(employeeModel);
+            SqlCommand command = new SqlCommand("SpAddEmployeeDetails", this.sqlConnection);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@Name", employeeDetails.Name);
+            command.Parameters.AddWithValue("@Salary", employeeDetails.Salary);
+            command.Parameters.AddWithValue("@Start", employeeDetails.Start);
+            command.Parameters.AddWithValue("@Gender", employeeDetails.Gender);
+            command.Parameters.AddWithValue("@PhoneNumber", employeeDetails.PhoneNumber);
+            command.Parameters.AddWithValue("@Address", employeeDetails.Address);
+            command.Parameters.AddWithValue("@Department", employeeDetails.Department);
+            command.Parameters.AddWithValue("@Basic_Pay", employeeDetails.Basic_Pay);
+            command.Parameters.AddWithValue("@Deductions", employeeDetails.Deductions);
+            command.Parameters.AddWithValue("@Taxable_Pay", employeeDetails.Taxable_Pay);
+            command.Parameters.AddWithValue("@Income_Tax", employeeDetails.Income_Tax);
+            command.Parameters.AddWithValue("@Net_Pay", employeeDetails.Net_Pay);
+            this.sqlConnection.Open();
+            /// It used to executing queries that does not return any data.
+            var result = command.ExecuteNonQuery();
+            this.sqlConnection.Close();
         }
 
         /// <summary>
@@ -104,8 +100,28 @@ namespace EmployeePayrollServiceWithDataBase
                 Task thread = new Task(() =>
                 {
                     Console.WriteLine("Employee being added: " + employeeData.Name);
-                    Console.WriteLine("Current thread Id: " + Thread.CurrentThread.ManagedThreadId);
-                    this.AddEmployee(employeeData);
+                    //Console.WriteLine("Current thread Id: " + Thread.CurrentThread.ManagedThreadId);
+                    this.AddEmployeePayroll(employeeData);
+                    Console.WriteLine("Employee Added:  " + employeeData.Name);
+                });
+                thread.Start();
+            });
+        }
+
+        /// <summary>
+        /// UC11 Using threads
+        /// Adds the employee payroll in to database using thread.
+        /// </summary>
+        /// <param name="employeePayrollList">The employee payroll list.</param>
+        public void AddEmployeePayrollInToDataBaseUsingThread(List<EmployeeModel> employeePayrollList)
+        {
+            employeePayrollList.ForEach(employeeData =>
+            {
+                Task thread = new Task(() =>
+                {
+                    Console.WriteLine("Employee being added: " + employeeData.Name);
+                    //Console.WriteLine("Current thread Id: " + Thread.CurrentThread.ManagedThreadId);
+                    this.AddEmployeeInToDataBase(employeeData);
                     Console.WriteLine("Employee Added:  " + employeeData.Name);
                 });
                 thread.Start();
